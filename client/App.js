814216cac8194dbccbcf40a6cf9e3ea4ad6d23be
William Barkoff <william@barkoffusa.com>
2021-09-25 14:16:35 -0400

    Get location from user

--- client/App.js
+++ client/App.js
@@ -4,14 +4,39 @@ import { FancyMap } from "./components/FancyMap"
 import { StyleSheet, View } from 'react-native';
 import Constants from 'expo-constants';
 import GooglePlacesInput from './components/PlaceInput';
-
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = React.useState();
+  const [hasLocation, setHasLocation] = React.useState(false);
+  const [destination, setDestination] = React.useState(null)
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Inneroute needs access to your location for routing purposes.');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setHasLocation(true)
+    })();
+  }, []);
+
+  function fetchLocation(details, data) {
+    console.log(details, data)
+    setDestination({ details, data })
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <GooglePlacesInput />
-      <FancyMap />
+      <View style={styles.input}>
+        <GooglePlacesInput setLocation={fetchLocation} />
+      </View>
+      <FancyMap styles={styles.fancyMap} currentLocation={location} destination={destination} />
     </View>
   );
 }
@@ -19,10 +44,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Constants.statusBarHeight + 10,
-    backgroundColor: '#ecf0f1',
     flexGrow: 1
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
+  input: {
+    zIndex: 2,
+    padding: 10,
+    position: "absolute",
+    width: "100%",
+    paddingTop: 20 + Constants.statusBarHeight
+  },
+  fancyMap: {
+    zIndex: 1
+  }
 });
