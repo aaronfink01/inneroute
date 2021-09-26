204c49fea60d540a1fee1cbf83cb58b94022d541
Esther Wang <ew375@cornell.edu>
2021-09-25 20:03:13 -0400

    statler

--- server/internalmaps.js
+++ server/internalmaps.js
@@ -46,6 +46,112 @@ export const data = {
       null,
       "Walk straight to the down staircase."
     ]
+  ],
+  "Statler": [
+    [
+      null,
+      "Walk forward to the bottom of the stairs",
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    [
+      "Walk forward - the exit is directly in front of you.",
+      null,
+      null,
+      "Go up the stairs",
+      null,
+      null,
+      null,
+      "Go up the stairs",
+      "Go straight past the stairs away from the doors. Take a left through the" +
+      " double glass doors and continue past Mac's Cafe and the hotel laundry, " +
+      "taking a left at the single wooden door. Take an immediate right away from " +
+      "Terrace, and exit through the Terrace main entrance."
+    ],
+    [
+      null,
+      null,
+      null,
+      "Turn right. Walk past the curved bench towards the Statler Auditorium. ",
+      "After entering, make a left, and walk straight. Exit immediately to the left of the atrium",
+      null,
+      null,
+      null,
+      null
+    ],
+    [
+      null,
+      "Go down the stairs.",
+      "Go away from the staircase past the curved bench. Make a left. The exit will be on your right.",
+      null,
+      "Walk away from the staircase past the curved bench. The exit is immediately to the left of the atrium. ",
+      null,
+      null,
+      "Walk past the staircase towards the edge of the balcony. ",
+      null
+    ],
+    [
+      null,
+      null,
+      "After entering, walk straight until you see an exit on your left. ",
+      "Make a left and walk past the curved bench towards the Statler Auditorium. ",
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      "Enter the staircase. Go down one flight and exit the building. ",
+      "Walk away from the staircase towards the balcony. ",
+      null
+    ],
+    [
+      null,
+      null,
+      null,
+      null,
+      null,
+      "Make an immediate right and go up one flight of stairs. Exit the staircase. ",
+      null,
+      null,
+      null
+    ],
+    [
+      null,
+      "Go down the stairs. ",
+      null,
+      "Walk away from the balcony towards the Statler Auditorium. ",
+      null,
+      "Make a left when facing the balcony and walk straight until you see a staircase. ",
+      null,
+      null,
+      null
+    ],
+    [
+      null,
+      "Walk in through Terrace main entrance. Turn left at the elevator. Go through single wooden " +
+      "door and make an immediate right. Continue down the hallway, past hotel laundry and Mac's Cafe " +
+      "until you reach the bottom of the staircase. ",
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]
   ]
 }
 
